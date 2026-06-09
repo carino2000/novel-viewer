55625d62640b2cb0fbbac3f7c5d5abca2018d4c5
carino9231 <carino9231@gmail.com>
2026-06-09 12:00:54 +0900

    Initial commit

---
+++ src/main/java/novel_viewer/NovelViewerApplication.java
@@ -0,0 +1,13 @@
+package novel_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novel_viewer/NovelViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package novel_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
